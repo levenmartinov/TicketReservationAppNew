0110d08fd032412019bb8a54b3a7d31d01b75105
levenmartinov <EmailAdresiniz>
2024-10-14 23:59:09 +0100

    TicketReservationAppNew

--- src/TicketReservationAppNew/Passenger.java
+++ src/TicketReservationAppNew/Passenger.java
@@ -1,4 +1,38 @@
 package TicketReservationAppNew;
 
 public class Passenger {
+
+    private  String name;
+    private int age;
+
+    //Constructor
+
+    public Passenger(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getter methods
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // setter methods
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+
 }
